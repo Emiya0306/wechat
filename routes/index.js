46761992cc049888b91fe919afb0139398fb07e6
roger <worfandsheep@qq.com>
2016-03-02 11:09:45 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var config = {
 router.use('/', wechat(config, function (req, res, next) {
     // 微信输入信息都在req.weixin上
     var message = req.weixin;
-    console.log(123);
+    console.log(req.weixin);
     if (message.FromUserName === 'diaosi') {
         // 回复屌丝(普通回复)
         res.reply('hehe');
